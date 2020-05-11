a0c52af7cf50e70b96024a38d2ac87a4589b8837
jwilson-hall <40354936+jwilson-hall@users.noreply.github.com>
2020-05-11 04:43:05 +0100

    Update Matrices_Functions.py

--- Matrices_Functions.py
+++ Matrices_Functions.py
@@ -1,4 +1,10 @@
-
+rectangle = [
+    [ 0, 0],
+    [300, 0],
+    [300, 100],
+    [0, 100],
+    [0, 0]
+]
 matrixD = [
     [1,2,3],
     [4,5,6]
@@ -64,3 +70,22 @@ def multiplyMatrices(m1,m2):
                     m3[i][j] += m1[i][k] * m2[k][j]
         print(m3)
         return m3
+ 
+
+from graphics import *
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+win = GraphWin("Matrix Graphics", SCREEN_WIDTH, SCREEN_HEIGHT)
+win.setBackground("white")
+
+def drawMatrix(matrix,mult):
+    #multiplier will make it bigger,
+    if mult<1:
+        mult=1    
+    for item in range(0,len(matrix)):
+        if item==len(matrix)-1:
+            Line(Point(matrix[item][0]*mult,matrix[item][1]*mult),Point(matrix[0][0]*mult,matrix[0][1]*mult)).draw(win)
+        else:
+            Line(Point(matrix[item][0]*mult,matrix[item][1]*mult),Point(matrix[item+1][0]*mult,matrix[item+1][1]*mult)).draw(win) 
+            
+drawMatrix(rectangle,2)
